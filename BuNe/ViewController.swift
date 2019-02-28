2abb1f5616858104212bbb1ae693c179e8ac78d1
yusufbasol <basolyusuff@gmail.com>
2019-02-28 23:19:33 +0300

    Initial Commit

---
+++ BuNe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BuNe
+//
+//  Created by yusufbasol on 28.02.2019.
+//  Copyright © 2019 com.yusufbasol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
